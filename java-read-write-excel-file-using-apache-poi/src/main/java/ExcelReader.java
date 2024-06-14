87d90823d3549998af289e4e19004634550afd72
Shareef <163128705+mohammadshareefc@users.noreply.github.com>
2024-06-14 17:44:21 +0530

    Add files via upload
    Read_Excel

---
+++ java-read-write-excel-file-using-apache-poi/src/main/java/ExcelReader.java
@@ -0,0 +1,112 @@
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+
+public class ExcelReader {
+    public static final String SAMPLE_XLS_FILE_PATH = "./sample-xls-file.xls";
+    public static final String SAMPLE_XLSX_FILE_PATH = "./sample-xlsx-file.xlsx";
+
+    public static void main(String[] args) throws IOException, InvalidFormatException {
+
+        Workbook workbook = WorkbookFactory.create(new File(SAMPLE_XLSX_FILE_PATH));
+
+        System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
+
+       
+       
+        Iterator<Sheet> sheetIterator = workbook.sheetIterator();
+        System.out.println("Retrieving Sheets using Iterator");
+        while (sheetIterator.hasNext()) {
+            Sheet sheet = sheetIterator.next();
+            System.out.println("=> " + sheet.getSheetName());
+        }
+
+      
+        System.out.println("Retrieving Sheets using for-each loop");
+        for(Sheet sheet: workbook) {
+            System.out.println("=> " + sheet.getSheetName());
+        }
+
+       
+        System.out.println("Retrieving Sheets using Java 8 forEach with lambda");
+        workbook.forEach(sheet -> {
+            System.out.println("=> " + sheet.getSheetName());
+        });
+
+       
+        Sheet sheet = workbook.getSheetAt(0);
+
+      
+        DataFormatter dataFormatter = new DataFormatter();
+
+     
+        System.out.println("\n\nIterating over Rows and Columns using Iterator\n");
+        Iterator<Row> rowIterator = sheet.rowIterator();
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+
+          
+            Iterator<Cell> cellIterator = row.cellIterator();
+
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                String cellValue = dataFormatter.formatCellValue(cell);
+                System.out.print(cellValue + "\t");
+            }
+            System.out.println();
+        }
+
+   
+        System.out.println("\n\nIterating over Rows and Columns using for-each loop\n");
+        for (Row row: sheet) {
+            for(Cell cell: row) {
+                String cellValue = dataFormatter.formatCellValue(cell);
+                System.out.print(cellValue + "\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n\nIterating over Rows and Columns using Java 8 forEach with lambda\n");
+        sheet.forEach(row -> {
+            row.forEach(cell -> {
+                printCellValue(cell);
+            });
+            System.out.println();
+        });
+
+       
+        workbook.close();
+    }
+
+    private static void printCellValue(Cell cell) {
+        switch (cell.getCellTypeEnum()) {
+            case BOOLEAN:
+                System.out.print(cell.getBooleanCellValue());
+                break;
+            case STRING:
+                System.out.print(cell.getRichStringCellValue().getString());
+                break;
+            case NUMERIC:
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    System.out.print(cell.getDateCellValue());
+                } else {
+                    System.out.print(cell.getNumericCellValue());
+                }
+                break;
+            case FORMULA:
+                System.out.print(cell.getCellFormula());
+                break;
+            case BLANK:
+                System.out.print("");
+                break;
+            default:
+                System.out.print("");
+        }
+
+        System.out.print("\t");
+    }
+}
